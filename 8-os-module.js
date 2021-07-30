bc061ac179cd0c79f11ac95d515e8f82b9b5e474
simon <smnthendu56@gmail.com>
2021-07-30 16:22:46 +0300

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,15 @@
+const os = require('os')
+//info about user
+const user = os.userInfo()
+
+//system up time
+console.log(`The System uptime is ${os.uptime()}seconds`)
+
+
+const currentOS ={
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem(),
+}
+console.log(currentOS)
